1c45c66d0123e6209e774a695ebc591f00dfde80
AtharvaC1810 <saplatharva@gmail.com>
2025-11-23 20:02:52 +0530

    changed app module

--- backend-app/src/app.module.ts
+++ backend-app/src/app.module.ts
@@ -1,44 +1,25 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { User } from './users/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      isGlobal: true,
+      isGlobal: true,  
     }),
-
     TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => {
-        const host = configService.get<string>('DATABASE_HOST');
-        const port = configService.get<number>('DATABASE_PORT');
-        const username = configService.get<string>('DATABASE_USER');
-        const password = configService.get<string>('DATABASE_PASSWORD');
-        const database = configService.get<string>('DATABASE_NAME');
-
-        if (!host || !port || !username || !password || !database) {
-          throw new Error('Database configuration is missing in .env');
-        }
-
-        return {
-          type: 'postgres',
-          host,
-          port: +port,
-          username,
-          password,
-          database,
-          entities: [User],
-          synchronize: true,
-        };
-      },
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: true, 
+        autoLoadEntities: true,
+      }),
     }),
-
-    UsersModule,
     AuthModule,
   ],
 })
