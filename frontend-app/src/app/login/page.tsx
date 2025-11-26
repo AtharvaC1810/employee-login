843898cce22afcc40c97cca128db4edf9eaeb954
AtharvaC1810 <saplatharva@gmail.com>
2025-11-26 15:25:31 +0530

    updated env on frontend

--- frontend-app/src/app/login/page.tsx
+++ frontend-app/src/app/login/page.tsx
@@ -20,7 +20,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
