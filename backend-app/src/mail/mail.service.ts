707d1d8b8c25917a3bc09f072499718e225d1cdd
AtharvaC1810 <saplatharva@gmail.com>
2025-12-05 18:10:51 +0530

    changed api

--- backend-app/src/mail/mail.service.ts
+++ backend-app/src/mail/mail.service.ts
@@ -6,24 +6,24 @@ export class MailService {
   private logger = new Logger(MailService.name);
 
   constructor() {
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
   }
 
   async sendMail(to: string, subject: string, html: string, text?: string) {
-    const msg = {
-      to,
-      from: process.env.MAIL_FROM!,
-      subject,
-      text: text || undefined,
-      html,
-    };
-
     try {
-      const response = await sgMail.send(msg);
+      const msg = {
+        to,
+        from: process.env.EMAIL_FROM!,
+        subject,
+        text: text || undefined,
+        html,
+      };
+
+      await sgMail.send(msg);
       this.logger.log(`Mail sent to ${to}`);
-      return response;
+      return true;
     } catch (err: any) {
-      this.logger.error(`Failed to send mail: ${err.message}`);
+      this.logger.error(`SendGrid Error: ${err.message}`);
       throw err;
     }
   }
@@ -33,9 +33,8 @@ export class MailService {
       <div style="font-family: sans-serif; line-height:1.4;">
         <h2>Password reset request</h2>
         <p>Hello ${name || 'user'},</p>
-        <p>We received a request to reset your password. Click the link below to reset your password. This link will expire in ${process.env.RESET_TOKEN_TTL_MINUTES || 30} minutes.</p>
+        <p>Click below to reset your password:</p>
         <p><a href="${resetUrl}">Reset password</a></p>
-        <p>If you didn't request this, you can ignore this email.</p>
       </div>
     `;
   }
